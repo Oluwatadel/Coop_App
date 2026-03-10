069213aa93a2e574de6a370a5264f3babe1954b0
Gabriel Oluwatobi Adelesi <111093334+Oluwatadel@users.noreply.github.com>
2026-03-10 12:57:08 +0100

    changes

--- src/Services/Interfaces/IAssociationService.cs
+++ src/Services/Interfaces/IAssociationService.cs
@@ -4,7 +4,7 @@ namespace CoopApplication.Services.Interfaces
 {
     public interface IAssociationService
     {
-        Task<Association?> CreateAssociationAsync(string name, string? description, CancellationToken cancellationToken);
+        Task<Association?> CreateAssociationAsync(string name, string description, CancellationToken cancellationToken);
         Task<Association> UpdateAssociationAsync(Guid associationId, string name, CancellationToken cancellationToken);
         Task<Association> GetAssociationByNameAsync(string name, CancellationToken cancellationToken);
         Task<Association> GetAssociationByIdAsync(Guid id, CancellationToken cancellationToken);
