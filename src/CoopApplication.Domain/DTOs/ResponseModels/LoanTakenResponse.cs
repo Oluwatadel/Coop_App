9505c6cc5e0396ed909ee4872a7665794ca09ab9
Gabriel Oluwatobi Adelesi <111093334+Oluwatadel@users.noreply.github.com>
2026-03-12 11:25:33 +0100

    created staging branch

--- src/CoopApplication.Domain/DTOs/ResponseModels/LoanTakenResponse.cs
+++ src/CoopApplication.Domain/DTOs/ResponseModels/LoanTakenResponse.cs
@@ -10,23 +10,14 @@ namespace CoopApplication.Domain.DTOs.ResponseModels
     public record LoanTakenResponse
     {
         public Guid Id { get; set; }
-
         public Guid UserId { get; set; }
-
         public string LoanTypeName { get; set; }
-
         public decimal PrincipalAmount { get; set; }
-
         public decimal TotalRepaymentAmount { get; set; }
-
         public decimal MonthlyPaymentAmount { get; set; }
-
         public decimal BalanceRemaining { get; set; }
-
         public LoanStatus Status { get; set; }
-
         public DateTime StartDate { get; set; }
-
         public DateTime EndDate { get; set; }
     }
 }
