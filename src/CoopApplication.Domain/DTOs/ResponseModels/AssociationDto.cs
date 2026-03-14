0f1c59ea8fe339fea5a630e2a6a7641f67ec6e73
Gabriel Oluwatobi Adelesi <111093334+Oluwatadel@users.noreply.github.com>
2026-03-14 16:51:48 +0100

    userdashboard detail increased

--- src/CoopApplication.Domain/DTOs/ResponseModels/AssociationDto.cs
+++ src/CoopApplication.Domain/DTOs/ResponseModels/AssociationDto.cs
@@ -1,7 +1,12 @@
 ﻿namespace CoopApplication.Domain.DTOs.ResponseModels
 {
-    public record AssociationDto(
-        Guid Id, string Name, 
-        string? Description, int MemberCount
-     );
+    //public record AssociationDto(
+    //    Guid Id, string Name, 
+    //    string? Description, int MemberCount
+    // );
+
+    public record AssociationDto(Guid Id, string Name, string Description, int TotalMembers, AssociationLoanSummary AssociationSummary);
+
+    public record AssociationLoanSummary(int loanCount,
+        decimal TotalPrincipal, decimal TotalBalance, decimal TotalSavings);
 }
