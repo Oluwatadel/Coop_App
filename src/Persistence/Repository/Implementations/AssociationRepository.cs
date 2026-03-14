26b61f1f0c355e59b020e5e02bcbb67e16bded5f
Gabriel Oluwatobi Adelesi <111093334+Oluwatadel@users.noreply.github.com>
2026-03-14 17:04:53 +0100

    changes in AsociationDto

--- src/Persistence/Repository/Implementations/AssociationRepository.cs
+++ src/Persistence/Repository/Implementations/AssociationRepository.cs
@@ -35,19 +35,17 @@ namespace CoopApplication.Persistence.Repository.Implementations
             var query = from association in context.Associations
                         join user in context.Users
                         on association.Id equals user.AssociationId into userGroup
-                        let userId = userGroup.Select(u => u.Id)
-                        let loans = context.LoanTaken.Where(l => userId.Contains(l.UserId))
+                        let memberIds = userGroup.Select(u => u.Id)
                         select new AssociationDto(
                     association.Id,
                     association.Name,
                     association.Description,
                     userGroup.Count(),
                     new AssociationLoanSummary(
-                        loans.Count(),
-                        loans.Sum(l => (decimal?)l.PrincipalAmount) ?? 0,
-                        loans.Sum(l => (decimal?)l.BalanceRemaining) ?? 0,
-                        context.Accounts.Where(a => userId.Contains(a.UserId))
-                            .Sum(a => (decimal?)a.SavingsBalance) ?? 0
+                        context.LoanTaken.Count(l => memberIds.Contains(l.UserId)),
+                        context.LoanTaken.Where(l => memberIds.Contains(l.UserId)).Sum(l => (decimal?)l.PrincipalAmount) ?? 0,
+                        context.LoanTaken.Where(l => memberIds.Contains(l.UserId)).Sum(l => (decimal?)l.BalanceRemaining) ?? 0,
+                        context.Accounts.Where(l => memberIds.Contains(l.UserId)).Sum(l => (decimal?)l.SavingsBalance) ?? 0
                     )
                 );
 
